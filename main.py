27a0b0f57834cb2389a2ecc5d1a6fce265a8ada6
KarapetGaranyan <98229085+KarapetGaranyan@users.noreply.github.com>
2025-04-10 07:34:06 +0300

    add new func

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ def get_max(a, b, c):
 def celsius_to_fahrenheit(celsius):
     """Конвертирует температуру из Цельсия в Фаренгейт"""
     return (celsius * 9/5) + 32
+
+# Примеры использования
+print(is_even(4))  # Выведет True
+print(is_even(7))  # Выведет False
+
+print(get_max(10, 5, 8))  # Выведет 10
+
+print(celsius_to_fahrenheit(0))   # Выведет 32.0
+print(celsius_to_fahrenheit(100)) # Выведет 212.0
